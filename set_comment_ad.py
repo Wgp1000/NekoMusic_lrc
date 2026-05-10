7b4b47219b02110c35cec3c52803022bc532ba72
NyaNyagulugulu <support@cnmsb.xin>
2026-05-10 23:15:14 +0800

    移除多余换行符

--- set_comment_ad.py
+++ set_comment_ad.py
@@ -40,7 +40,7 @@ PUBLISHER = "music.cnmsb.xin"
 # 内嵌歌词（LRC 时间轴）横幅，写在已有歌词最前
 LYRICS_BANNER = (
     "[00:00.05]资源来自Neko云音乐 Resources from Neko Cloud Music\n"
-    "[00:00.10]获取更多无损音乐https://music.cnmsb.xin/ Get more lossless music at https://music.cnmsb.xin/\n"
+    "[00:00.10]获取更多无损音乐https://music.cnmsb.xin/ Get more lossless music at https://music.cnmsb.xin/" \
 )
 LYRICS_BANNER_FIRST = "[00:00.05]资源来自Neko云音乐 Resources from Neko Cloud Music"
 
